2b9901907153c6bcb1120aeedf71a6ba98edb3b2
yusraalnas <45268417+yusraalnas@users.noreply.github.com>
2022-06-17 00:50:44 +0100

    Update SummativeSums.java

--- Assessment-basic/src/SummativeSums.java
+++ Assessment-basic/src/SummativeSums.java
@@ -8,11 +8,13 @@ public class SummativeSums {
     }
 
     public SummativeSums(){
+        //set up arrays to pass in
         int[] array1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
         int[] array2 = { 999, -60, -77, 14, 160, 301 };
         int[] array3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
                 140, 150, 160, 170, 180, 190, 200, -99 };
 
+        //Passing number for output message and array so can be added up
         addUp(1, array1);
         addUp(2, array2);
         addUp(3, array3);
@@ -21,7 +23,7 @@ public class SummativeSums {
     public static void addUp(int number, int[] array){
         int total = 0;
 
-        for (int i: array){
+        for (int i: array){ //goes through every element in array
             total += i;
         }
         System.out.println("#" + number + " Array Sum: " + total);
